1cf498a3626777ba889b38e5947ba62ee8c5a45d
Carlos <np.carlos@gmail.com>
2020-07-08 13:20:04 -0500

    Mejoras en el enrutamiento. Uso de shortid

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const bodyParser = require('body-parser')
 const apiRoutes = require('./app/routes/api')
 const app = express()
 
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 app.use('', apiRoutes)
 
-app.get('/', (req, res) => {
-    res.send("welcome to server");
+app.get('/', (request, response) => {
+    response.send("welcome to server");
 });
 
 
