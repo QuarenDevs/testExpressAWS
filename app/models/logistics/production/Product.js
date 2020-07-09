516a491d28e00117d8c0823f0072930de5338028
Carlos <np.carlos@gmail.com>
2020-07-08 23:34:04 -0500

    Swagger Doc en modelo Producto

--- app/models/logistics/production/Product.js
+++ app/models/logistics/production/Product.js
@@ -3,6 +3,32 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
+/**
+ * @swagger
+ * components:
+ *      schemas:
+ *          Product:
+ *              type: object
+ *              properties:
+ *                  sid:
+ *                      type: string 
+ *                      description: Short id of the object
+ *                  name:
+ *                      type: string 
+ *                      description: Name of the product.
+ *                  size:
+ *                      type: number
+ *                      minium: 0
+ *                      description: Height of the product.
+ *                  unitaryPrice:
+ *                      type: number
+ *                      minium: 0
+ *                      description: Unitary price product.
+ *                  description:
+ *                      type: 
+ *                      minium: 0
+ *                      description: Height of the product.
+ */
 const ProductSchema = Schema({
     name: String,
     //sid: {'type': String, 'default': shortid.generate},
