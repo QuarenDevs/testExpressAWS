616017be76e7d2ff43e3cb1e3fbdce550f7639f9
Carlos <np.carlos@gmail.com>
2020-07-08 20:32:05 -0500

    Deshacer shortid en modelo Product

--- app/models/logistics/production/Product.js
+++ app/models/logistics/production/Product.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
-var shortid = require('shortid');
+//var shortid = require('shortid');
 
 const Schema = mongoose.Schema
 
 const ProductSchema = Schema({
     name: String,
-    sid: {'type': String, 'default': shortid.generate},
+    //sid: {'type': String, 'default': shortid.generate},
     size: Number,
     unitaryPrice: Number,
     imgURL: String,
