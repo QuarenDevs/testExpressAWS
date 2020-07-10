f5b523fadad805d27a2509219d18e6149ffca4de
Carlos <np.carlos@gmail.com>
2020-07-10 16:37:11 -0500

    Inclusión de cors en el package.json

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const Joi = require('joi');
 const bodyParser = require('body-parser')
 const apiRoutes = require('./app/routes/api')
+const cors = require('cors')
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: false})) 
 app.use(bodyParser.json()) 
+app.use(cors())
 
 app.use('', apiRoutes)
 
